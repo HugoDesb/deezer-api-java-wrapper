0dbe0d3c59dcb5c45466ca83dafdd15d0e54c88a
hugodesb <hugo.desbiolles@gmail.com>
2020-12-06 19:35:33 +0100

    deserializers

---
+++ src/main/java/com/wrapper/deezer/deserializer/DateYearMonthDayDeserializer.java
@@ -0,0 +1,27 @@
+package com.wrapper.deezer.deserializer;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class DateYearMonthDayDeserializer implements JsonDeserializer<Date> {
+
+    @Override
+    public Date deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        String date = jsonElement.getAsJsonPrimitive().getAsString();
+        String pattern = "yyyy-MM-dd";
+        try {
+            return (new SimpleDateFormat("yyyy-MM-dd")).parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return Date.from(Instant.MIN);
+    }
+}
